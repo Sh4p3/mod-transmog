8bfa285d69d97936c8c1e4364612acb614bdf5ac
Sh4p3 <gheysens@hotmail.fr>
2026-06-07 22:38:51 +0200

    Allow portable transmog without plus

--- src/cs_transmog.cpp
+++ src/cs_transmog.cpp
@@ -288,15 +288,10 @@ public:
             return true;
         }
 
-        if (!sTransmogrification->IsTransmogPlusEnabled)
-        {
-            handler->SendErrorMessage("The portable transmogrification NPC is a plus feature. Plus features are currently disabled.");
-            return true;
-        }
-
         Player* player = PlayerIdentifier::FromSelf(handler)->GetConnectedPlayer();
 
-        if (!sTransmogrification->IsPlusFeatureEligible(player->GetGUID(), PLUS_FEATURE_PET))
+        if (sTransmogrification->IsTransmogPlusEnabled &&
+            !sTransmogrification->IsPlusFeatureEligible(player->GetGUID(), PLUS_FEATURE_PET))
         {
             handler->SendErrorMessage("You are not eligible for the portable transmogrification NPC. Please check your subscription level.");
             return true;
